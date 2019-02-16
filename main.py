82b67864b1e85e20b2f898a1002acbc9c28f428b
mukushevzhanarbek@gmail.com <mukushevzhanarbek@gmail.com>
2019-02-16 15:24:29 +0600

    Seminar3

---
+++ main.py
@@ -0,0 +1,23 @@
+from linked_class import ArrayStack
+
+class Stack:
+    def __init__(self):
+        self._theItems = list()
+
+    def isEmpty(self):
+        return len(self) == 0
+
+    def __len__(self):
+        return len(self._theItems)
+
+    def peek(self):
+        assert not self.isEmpty(), "Cannot peek at an empty stack"
+        return self._Items[-1]
+
+    def pop(self):
+        assert not self.isEmpty(), "Cannot peek at an empty stack"
+        return self._theItems.pop()
+
+    def push(self,item):
+        self._theItems.append(item)
+    
